129d7eb2686b0f3d0797274037deb9fb973f0497
aditya kelekar <adityakelekar@yahoo.com>
2026-06-04 12:26:48 +0300

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,44 @@
+import { useState, useEffect } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+
+import SignInSide from './sign-in-side/SignInSide'
+
+// import SignInSide from './sign-in-side/SignInSide'
+
+function App() {
+  const [count, setCount] = useState(0)
+  const [currentTime, setCurrentTime] = useState(0);
+
+  useEffect(() => {
+    fetch('/api/time').then(res => res.json()).then(data => {
+      setCurrentTime(data.time);
+    });
+  }, []);
+
+  return (
+    <>
+      <div>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>The current time is {new Date(currentTime * 1000).toLocaleString()}.</p>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+      <SignInSide />
+    </>
+  )
+}
+
+export default App
